792d74023b7e1846616daec79ff117d5a1b9964b
Achuthan77 <achut637@gmail.com>
2025-08-28 20:14:38 +0530

    Create countstrings.java

---
+++ countstrings.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc=new Scanner(System.in);
+        String s=sc.nextLine();
+        int cnt=0;
+        for(int i=0;i<s.length();i++)
+        {
+            char ch=s.charAt(i);
+            if(!Character.isLetterOrDigit(ch)) {
+            cnt++;
+            }
+        }
+        System.out.println(cnt);
+        
+    }
+}
